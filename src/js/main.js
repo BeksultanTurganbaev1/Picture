15b033193dfa6d76b4479103768a63deb7e77266
Beksultan Turganbaev <beksultan.turganbaev.kk@gmail.com>
2024-02-03 09:49:18 +0300

    Add sliders.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,12 @@ var Promise = require('es6-promise-polyfill').Promise;
 import 'nodelist-foreach-polyfill';
 
 import modals from './modules/modals';
+import sliders from './modules/sliders';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     modals();
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
 });
