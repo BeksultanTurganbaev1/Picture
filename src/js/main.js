6f2a87a1c1d91f137b80e32eadf5c990982be2f6
Beksultan Turganbaev <beksultan.turganbaev.kk@gmail.com>
2024-02-05 09:49:04 +0300

    Add checkTextInputs.js Let it be written in the Cyrillic alphabet

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import modals from './modules/modals';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -15,4 +16,6 @@ window.addEventListener('DOMContentLoaded', () => {
     sliders('.main-slider-item', 'vertical');
     forms();
     mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
 });
