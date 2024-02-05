bc563f38c855f5ac62fc60749981b926e9c6dd59
Beksultan Turganbaev <beksultan.turganbaev.kk@gmail.com>
2024-02-05 07:32:01 +0300

    Add forms.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 var Promise = require('es6-promise-polyfill').Promise;
 import 'nodelist-foreach-polyfill';
+// import '../index.html';
 
 import modals from './modules/modals';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -10,4 +12,6 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
+    forms();
+    console.log(1)
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,45 @@
 'use strict';
-
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 let path = require('path');
 
 module.exports = {
     mode: 'development',
     entry: './src/js/main.js',
     output: {
-        path: path.resolve(__dirname, 'dist', 'js'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
+    // plugins: [
+    //   new HtmlWebpackPlugin({
+    //     template: path.resolve(__dirname, 'src', 'index.html')
+    //   })],
     watch: true,
 
     devtool: "source-map",
 
     module: {
         rules: [
-            {
-              test: /\.(?:js|mjs|cjs)$/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: [
-                    ['@babel/preset-env', { 
-                        targets: "defaults",
-                        debug: true,
-                        corejs: 3,
-                        useBuiltIns: "usage"  
-                    }]
-                  ]
-                }
+          // {
+          //   test: /\.html$/i,
+          //   loader: 'html-loader',
+          // },
+          {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { 
+                      targets: "defaults",
+                      debug: true,
+                      corejs: 3,
+                      useBuiltIns: "usage"  
+                  }]
+                ]
               }
             }
+          }
         ]
     }
 }
