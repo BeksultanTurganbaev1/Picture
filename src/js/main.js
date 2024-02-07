c0cf68ef92c29cf767e0870e359848b4398efe87
Beksultan Turganbaev <beksultan.turganbaev.kk@gmail.com>
2024-02-07 19:09:18 +0300

    Add calc.js

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
 import showMoreStyles from './modules/showMoreStyles';
+import calc from './modules/calc';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -19,5 +20,6 @@ window.addEventListener('DOMContentLoaded', () => {
     mask('[name="phone"]');
     checkTextInputs('[name="name"]');
     checkTextInputs('[name="message"]');
-    showMoreStyles('.button-styles', '#styles .row')
+    showMoreStyles('.button-styles', '#styles .row');
+    calc('#size', '#material', '#options', '.promocode', '.calc-price')
 });
